7b030306db3c120dace0b9d7b583dded12d7b409
JeremyMcdr <jeremymacadre@gmail.com>
2023-04-26 14:54:47 +0200

    Ajout de la modification

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('jeremymcdr_doormonitor', '286599', '258046722801Jm!', {
+const sequelize = new Sequelize('jeremymcdr_doormonitor', '286599', 'azertyuqsdfghjn,kjhgv n,lm84513ih675E', {
     host: 'mysql-jeremymcdr.alwaysdata.net',
     dialect: 'mysql',
 });
