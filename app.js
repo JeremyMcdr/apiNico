aa79d466b6988846a6bfe76e88e56f09d9f433fd
Jérémy <jeremymacadre@gmail.com>
2023-05-01 23:14:16 +0200

    Ajout Des nouvelles Tables

--- app.js
+++ app.js
@@ -28,16 +28,16 @@ const corsOptions = {
     optionsSuccessStatus: 200
 };
 
-const allowedOrigins = ['http://localhost:4200', '192.168.86.1'];
+const allowedOrigins = ['http://localhost:4200', '*'];
 
 app.use((req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Origin', origin);
     }
-    // Ajoutez les autres en-têtes CORS nécessaires ici
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
 
     if (req.method === 'OPTIONS') {
         res.sendStatus(200);
