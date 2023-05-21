1d0217e13f3415b8e68a548c277516eedefe9df4
Jérémy <jeremymacadre@gmail.com>
2023-05-21 20:44:54 +0200

    Ajout Des nouvelles Tables

--- routes/login.js
+++ routes/login.js
@@ -3,19 +3,19 @@ const router = express.Router();
 const UserApp = require('../models/usersApp');
 
 router.post('/', async (req, res) => {
-    const { firstName, password } = req.body;
+    const { pseudo, password } = req.body;
 
     try {
-        const user = await UserApp.findOne({ where: { firstName: firstName } });
+        const user = await UserApp.findOne({ where: { pseudo: pseudo } });
         if (!user) {
             res.json({ success: 0 });
             return;
         }
-
         // Comparaison du mot de passe envoyé avec celui stocké
-
         if (password === user.password) {
-            res.json({ success: 1 });
+            res.json({
+                success: 1,
+            });
         } else {
             res.json({ success: 0 });
         }
